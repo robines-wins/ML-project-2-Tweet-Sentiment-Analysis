1a03e2a87cd2bc6f5b018543b4739bcbf88bb137
Robin Solignac <robin.solignac@epfl.ch>
2016-12-14 13:34:23 +0100

    toy query for google word2vec

--- project_text_classification/cnn-text-classification-tf/word2vec.py
+++ project_text_classification/cnn-text-classification-tf/word2vec.py
@@ -3,5 +3,5 @@ import gensim
 model = gensim.models.Word2Vec.load_word2vec_format('../GoogleNews-vectors-negative300.bin.gz', binary=True)
 
 while True:
-	input_ = input()
-	print(input_ +": " (input_ in model.vocab)+"\n")
+	input_ = input("=>")
+	print(input_ , ": ", (input_ in model.vocab) ,"\n")
