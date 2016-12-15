6b3b57fb1e548086a3a74d568e241a0c030e84de
Hugo Moreau <moreauhugo@me.com>
2016-12-15 14:10:14 +0100

    Comments on vocabulary.py

--- project_text_classification/cnn-text-classification-tf/vocabulary.py
+++ project_text_classification/cnn-text-classification-tf/vocabulary.py
@@ -14,22 +14,52 @@ class Vocabulary(object):
 
 
 	def fit(self,strlist):
+		"""
+		Builds the vocabulariy from strlist. This will be done in two possible ways. 
+
+		1. if a pretrained vector set w2v was passed in the constructor then it will use it for each word to check 
+		if it is already in this set.
+			- if it is in this set, it will assign to a given word the pretrained vector
+			- otherwise it will assign a vector randomly generated.
+		2. if no pretrained vector set was passed then all words will be assigned to a randomly distributed vector.
+
+		IN : 
+		self :		the vocabulary object
+		strlist:	the list of of senetences for which we want to build a vocabulary
+		"""
 		for s in strlist:
 			for w in s.split():
+				# take care only of words not already processed
 				if w not in self.vocab:
+					# Store the index of the word (in the embedding) to the vocabulary
 					self.vocab[w]=self.vocsize
 					self.vocsize += 1
+
+					# Use the protocol explained above to assign vectors to words and put them in the embedding
 					if self.w2v != None and self.w2v.isin(w):
 						self.embeding.append(w2v.get(w))
 					else:
 						self.embeding.append(np.random.uniform(low=-1.0,high=1.0,size=self.dim))
 
 	def transform(self,strlist):
+		"""
+		Will use the vocabulary built by fit(.) to transform the list of tweets into a list of 
+		vectors. Each vector corresponds to a sentence and contains the index in the embedding 
+		of the given word (obtained from the vocabulary.
+
+		IN : 
+		self :		the vocabulary object
+		strlist:	the list of of senetences for which we want to build a vocabulary
+
+		OUT:
+		list of vectors of same size than the number of tweets
+		"""
 		tr = []
 		for s in strlist:
 			vec = np.zeros(self.maxlength,np.int64)
 			for idx,w in enumerate(s.split()):
 				if idx >= self.maxlength:
+					# If the sequence is too long (which should not happen in our case, then we just cut it rather than crashing)
 					break
 				if w in self.vocab:
 					vec[idx] = self.vocab[w]
@@ -37,10 +67,29 @@ class Vocabulary(object):
 		return tr
 
 	def fit_transform(self,strlist):
+		"""
+		Will first build the vocabulary and then return the list of vectors corresponding to each sentences
+
+		IN : 
+		self :		the vocabulary object
+		strlist:	the list of of senetences for which we want to build a vocabulary
+
+		OUT:
+		list of vectors of same size than the number of tweets
+		"""
 		self.fit(strlist)
 		return self.transform(strlist)
 
 	def embedingMatrix(self):
+		"""
+		Transforms the embeding list into a np array
+
+		IN : 
+		self :		the vocabulary object
+
+		OUT:
+		The embeding matrix
+		"""
 		return np.array(self.embeding)
 
 	def save(self, filename):
