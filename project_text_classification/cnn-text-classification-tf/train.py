124f0762a3d2dba4ddc6c1eb037b48c9010b898b
Hugo Moreau <moreauhugo@me.com>
2016-12-20 10:36:00 +0100

    Put back the baseline, delete the old readme

--- project_text_classification/cnn-text-classification-tf/train.py
+++ project_text_classification/cnn-text-classification-tf/train.py
@@ -192,16 +192,16 @@ if __name__ == "__main__":
 
     # Model Hyperparameters
     tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-    tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
-    tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
+    tf.flags.DEFINE_string("filter_sizes", "3", "Comma-separated filter sizes (default: '3,4,5')")
+    tf.flags.DEFINE_integer("num_filters", 64, "Number of filters per filter size (default: 128)")
     tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
     tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0.0)")
 
     # Training parameters
     tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
     tf.flags.DEFINE_integer("num_epochs", 1, "Number of training epochs (default: 200)")
-    tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
-    tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+    tf.flags.DEFINE_integer("evaluate_every", 2500, "Evaluate model on dev set after this many steps (default: 100)")
+    tf.flags.DEFINE_integer("checkpoint_every", 2500, "Save model after this many steps (default: 100)")
     tf.flags.DEFINE_string("w2v_path", "../tweetdatabase_word2vec", "path to precomputed word2vec vector")
 
     # Misc Parameters
